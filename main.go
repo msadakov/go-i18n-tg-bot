3abb59a8f7d416cc1244b6241c665683e7ecc867
Maksim Sadakov <pm@msadakov.me>
2018-12-09 17:38:45 +0300

    Add messages

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"flag"
 
 	tg "github.com/go-telegram-bot-api/telegram-bot-api"
+	"golang.org/x/text/feature/plural"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
 
 	"github.com/msadakov/go-i18n-tg-bot/log"
 )
@@ -12,6 +15,35 @@ var token string
 
 func init() {
 	flag.StringVar(&token, "token", "", "tg bot token")
+}
+
+func init() {
+	message.SetString(language.English,
+		"Hello, %s!\nMy name is %s. I'm a telegram bot.",
+		"Hello, %s!\nMy name is %s. I'm a telegram bot.")
+	message.SetString(language.Russian,
+		"Hello, %s!\nMy name is %s. I'm a telegram bot.",
+		"Привет, %s!\nМеня зовут %s. Я телегерам-бот.")
+
+	message.Set(language.English,
+		"There are %d day(s) left until the new year.",
+		plural.Selectf(1, "%d",
+			plural.One, "There are one day left until the new year!",
+			plural.Other, "There are %d days left until the new year."))
+	message.Set(language.Russian,
+		"There are %d day(s) left until the new year.",
+		plural.Selectf(1, "%d",
+			"=0", "С Новым годом!",
+			"=1", "До нового года остался один день!",
+			"<5", "До нового года осталось %d дня.",
+			"other", "До нового года осталось %d дней."))
+
+	message.SetString(language.English,
+		"Exchange rates",
+		"Exchange rates")
+	message.SetString(language.Russian,
+		"Exchange rates",
+		"Курсы валют")
 }
 
 func main() {
