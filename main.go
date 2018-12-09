ad0a564abc298d70dfceeb08d6e2c767c8ae2712
Maksim Sadakov <pm@msadakov.me>
2018-12-09 17:51:16 +0300

    Add command handlers

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"flag"
+	"time"
 
 	tg "github.com/go-telegram-bot-api/telegram-bot-api"
+	"golang.org/x/text/currency"
 	"golang.org/x/text/feature/plural"
 	"golang.org/x/text/language"
 	"golang.org/x/text/message"
@@ -85,4 +88,45 @@ func main() {
 			log.Error("Can't send a message: %v", err)
 		}
 	}
+}
+
+func cmdStart(p *message.Printer, usrName, botName string) string {
+	return p.Sprintf("Hello, %s!\nMy name is %s. I'm a telegram bot.", usrName, botName)
+}
+
+func cmdNewYear(p *message.Printer) string {
+	t := time.Now()
+	left := 365 - t.YearDay()
+
+	return p.Sprintf("There are %d day(s) left until the new year.", left)
+}
+
+type exchange struct {
+	from, to currency.Unit
+}
+
+var rates = map[exchange]float64{
+	{currency.USD, currency.RUB}: 66.42,
+	{currency.USD, currency.EUR}: 0.88,
+	{currency.RUB, currency.USD}: 0.015,
+	{currency.RUB, currency.EUR}: 0.013,
+	{currency.EUR, currency.RUB}: 75.60,
+	{currency.EUR, currency.USD}: 1.14,
+}
+
+func cmdRate(p *message.Printer, usrLng language.Tag) string {
+	unit, _ := currency.FromTag(usrLng)
+
+	var buf bytes.Buffer
+
+	p.Fprintf(&buf, "Exchange rates")
+	buf.WriteString(":\n")
+
+	for rate, amount := range rates {
+		if rate.to == unit {
+			p.Fprintf(&buf, "%s: %.2f\n", currency.Symbol(rate.from), amount)
+		}
+	}
+
+	return buf.String()
 }
